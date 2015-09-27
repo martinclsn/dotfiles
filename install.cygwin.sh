a8b53ae31bfbb64c40500e40a37063f3100628ec
U-CORP\martin.claesson <martin.claesson@jeppesensystems.com>
2015-09-28 01:17:06 +0200

    update

--- install.cygwin.sh
+++ install.cygwin.sh
@@ -1,3 +1,11 @@
 source install.sh
 
 link .bash_cygwin
+
+DOWNLOADS_DIR="$HOME/Downloads"
+[ -d "$DOWNLOADS_DIR" ] && rm -ri $DOWNLOADS_DIR
+ln -s $(cygpath -u $USERPROFILE)/Downloads $DOWNLOADS_DIR
+
+DESKTOP_DIR="$HOME/Desktop"
+[ -d "$DESKTOP_DIR" ] && rm -ri $DESKTOP_DIR
+ln -s $(cygpath -u $USERPROFILE)/Desktop $DESKTOP_DIR
